c5c1e3a7011fecca4e3cd62870b10aab946b1e03
623134636b3768756e643372 <86501450+623134636b3768756e643372@users.noreply.github.com>
2021-10-28 22:10:53 +0200

    Initial release

---
+++ BSCScan-Helper/CustomApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using BSCScan_Helper.Properties;
+using System;
+using System.Windows.Forms;
+
+namespace BSCScan_Helper
+{
+    public class CustomApplicationContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public CustomApplicationContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Text = "BSCScan helper",
+                Icon = (System.Drawing.Icon)Resources.ResourceManager.GetObject("icon"),
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Exit", Exit)
+            }),
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            //Program.TokenSource.Cancel();
+            Application.Exit();
+        }
+    }
+}
---
+++ BSCScan-Helper/Program.cs
@@ -0,0 +1,118 @@
+﻿using KeyboardHookClass;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace BSCScan_Helper
+{
+    static class Program
+    {
+        private static readonly NameValueCollection Config = ConfigurationManager.AppSettings;
+        private static Regex Supported;
+        private static string Server;
+        private static bool InstantCopy;
+        private static KeyboardHook hook;
+
+
+        [STAThread]
+        static void Main()
+        {
+            ReadConfig();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            RegisterHotkey();
+
+            Application.Run(new CustomApplicationContext());
+        }
+
+        private static void ReadConfig()
+        {
+            Server = Config.Get("Server");
+
+            InstantCopy = Convert.ToBoolean(Config.Get("UseInstantcopy"));
+
+            string _Supported = "(?'Transaction'0x[a-zA-Z0-9]{64})|(?'Address'0x[a-zA-Z0-9]{40})";
+
+            Supported = new Regex(_Supported, RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+        }
+
+        private static void RegisterHotkey()
+        {
+            hook = new KeyboardHook();
+            hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(Execute);
+            hook.RegisterHotKey(ModifierKeys.Win | ModifierKeys.NoRepeat, Keys.Escape);
+        }
+
+        private static void Execute(object sender, KeyPressedEventArgs e)
+        {
+            TriggerClipboardCopy();
+
+            if (Clipboard.ContainsText())
+            {
+                string Clipboard_Content = Clipboard.GetText(TextDataFormat.Text).Replace("\r\n", " ");
+
+                List<string> ProcessedItems = GenerateLinks(Clipboard_Content);
+
+                LaunchLinks(ProcessedItems);
+            }
+        }
+
+        private static void TriggerClipboardCopy()
+        {
+            if (InstantCopy)
+            {
+                Thread.Sleep(200);
+                SendKeys.Send("^c");
+                Thread.Sleep(200);
+            }
+        }
+
+        private static void LaunchLinks(List<string> ProcessedItems)
+        {
+            foreach (string Address in ProcessedItems)
+            {
+                Process.Start(Address);
+            }
+        }
+
+        private static List<string> GenerateLinks(in string Clipboard_Content)
+        {
+            List<string> ProcessedItems = new List<string>();
+
+            foreach (Match Item in Supported.Matches(Clipboard_Content))
+            {
+                string Link;
+                string Address = Item.Value;
+
+                if (Item.Groups["Address"].Success)
+                {
+                    Link = $"https://{Server}/address/{Address}";
+                }
+                else if (Item.Groups["Transaction"].Success)
+                {
+                    Link = $"https://{Server}/tx/{Address}";
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+
+                if (!ProcessedItems.Contains(Link))
+                {
+                    ProcessedItems.Add(Link);
+                }
+            }
+
+            return ProcessedItems;
+        }
+
+    }
+}
